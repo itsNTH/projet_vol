b3a596852c7ab09186b1c558111904f45e0428b1
Nathan-Edward SONGHO-MALONGA <SONGHO-MALONGA_Na@lprs.local>
2024-03-13 09:37:48 +0100

    th: ajout de la bdd

---
+++ src/bdd/projet_vol.sql
@@ -0,0 +1,129 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mer. 13 mars 2024 à 08:36
+-- Version du serveur : 5.7.36
+-- Version de PHP : 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `projet_vol`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `avion`
+--
+
+DROP TABLE IF EXISTS `avion`;
+CREATE TABLE IF NOT EXISTS `avion` (
+  `id_avion` int(11) NOT NULL AUTO_INCREMENT,
+  `modele` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_avion`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `fonction`
+--
+
+DROP TABLE IF EXISTS `fonction`;
+CREATE TABLE IF NOT EXISTS `fonction` (
+  `id_fonction` int(11) NOT NULL AUTO_INCREMENT,
+  `admin` tinyint(4) NOT NULL,
+  `client` tinyint(4) NOT NULL,
+  `pilote` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id_fonction`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reserver`
+--
+
+DROP TABLE IF EXISTS `reserver`;
+CREATE TABLE IF NOT EXISTS `reserver` (
+  `ref_utilisateur` int(11) NOT NULL,
+  `ref_vol` int(11) NOT NULL,
+  `annuler` int(11) NOT NULL,
+  KEY `fk_ref_utilisateurs` (`ref_utilisateur`),
+  KEY `fk_ref_vol` (`ref_vol`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `id_utilisateur` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) NOT NULL,
+  `prenom` varchar(100) NOT NULL,
+  `age` int(2) NOT NULL,
+  `ville` varchar(100) NOT NULL,
+  `ref_fonction` int(11) NOT NULL,
+  PRIMARY KEY (`id_utilisateur`),
+  KEY `fk_ref_fonction` (`ref_fonction`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `vol`
+--
+
+DROP TABLE IF EXISTS `vol`;
+CREATE TABLE IF NOT EXISTS `vol` (
+  `id_vol` int(11) NOT NULL AUTO_INCREMENT,
+  `destination` varchar(100) NOT NULL,
+  `heure_dep` datetime NOT NULL,
+  `heure_arr` datetime NOT NULL,
+  `ville_arr` varchar(100) NOT NULL,
+  `ref_avion` int(11) NOT NULL,
+  PRIMARY KEY (`id_vol`),
+  KEY `fk_ref_avion` (`ref_avion`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `reserver`
+--
+ALTER TABLE `reserver`
+  ADD CONSTRAINT `fk_ref_utilisateurs` FOREIGN KEY (`ref_utilisateur`) REFERENCES `utilisateur` (`id_utilisateur`),
+  ADD CONSTRAINT `fk_ref_vol` FOREIGN KEY (`ref_vol`) REFERENCES `vol` (`id_vol`);
+
+--
+-- Contraintes pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD CONSTRAINT `fk_ref_fonction` FOREIGN KEY (`ref_fonction`) REFERENCES `fonction` (`id_fonction`);
+
+--
+-- Contraintes pour la table `vol`
+--
+ALTER TABLE `vol`
+  ADD CONSTRAINT `fk_ref_avion` FOREIGN KEY (`ref_avion`) REFERENCES `avion` (`id_avion`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
